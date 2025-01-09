4cede41d112c2f1e38d6dada1e0d2512bc80aa56
Sudeep U <sudeepu@Sudeeps-MacBook-Air.local>
2025-01-09 23:59:35 +0530

    queues_stacks

---
+++ src/Reflection/Eagle.java
@@ -0,0 +1,13 @@
+package Reflection;
+
+public class Eagle {
+    public String breed;
+    private boolean canSwim;
+
+    public void fly(){
+        System.out.println("flying");
+    }
+    public void eat(){
+        System.out.println("eat");
+    }
+}
---
+++ src/Reflection/Main.java
@@ -0,0 +1,18 @@
+package Reflection;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class Main {
+    public static void main(String[] args) {
+        Class eagleClass = Eagle.class;
+        System.out.println(eagleClass.getName());
+        System.out.println(Modifier.toString(eagleClass.getModifiers()));
+
+        for(Method method:eagleClass.getMethods()){
+            System.out.println("name:"+method.getName());
+            System.out.println("return_type:"+method.getReturnType());
+            System.out.println("class:"+method.getDeclaringClass());
+        }
+    }
+}
